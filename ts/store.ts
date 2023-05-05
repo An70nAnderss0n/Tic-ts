da95068b0961cbfd1deb5c6ae8baafeeaf658baa
An70nAnderss0n <113804609+An70nAnderss0n@users.noreply.github.com>
2023-05-05 12:37:52 +0200

    First commit, sets up folders and typescript.

--- ts/store.ts
+++ ts/store.ts
@@ -1,42 +1,41 @@
+import type { Player } from './types';
+
 const initialValue = {
   currentGameMoves: [],
   history: {
     currentRoundGames: [],
-    allGames: []
-  }
+    allGames: [],
+  },
 };
 
+export default class Store extends EventTarget {´
 
-export default class Store extends EventTarget {
-
-
-  constructor(key, players) {
-    super();
-    this.players = players;
-    this.storageKey = key;
+  constructor(
+    private readonly storageKey: string, 
+    private readonly players: Player[]) 
+    {
+    super()
   }
-
 
   get stats() {
     const state = this.#getState();
 
     return {
       playerWithStats: this.players.map(player => {
-        const wins = state.history.currentRoundGames.filter(game => game.status.winner?.id === player.id).length;
+        const wins = state.history.currentRoundGames.filter(
+          game => game.status.winner?.id === player.id
+        ).length;
 
         return {
           ...player,
-          wins
-        }
+          wins,
+        };
       }),
-      ties: state.history.currentRoundGames.filter(game => game.status.winner === null).length
+      ties: state.history.currentRoundGames.filter(game => game.status.winner === null).length,
     };
   }
 
   get game() {
-
-
-
     const state = this.#getState();
 
     const currentPlayer = this.players[state.currentGameMoves.length % 2];
@@ -55,15 +54,16 @@ export default class Store extends EventTarget {
     let winner = null;
 
     for (const player of this.players) {
-      const selectedSquareIds = state.currentGameMoves.filter(move => move.player.id === player.id).map(move => move.squareId)
+      const selectedSquareIds = state.currentGameMoves
+        .filter(move => move.player.id === player.id)
+        .map(move => move.squareId);
 
       for (const pattern of winningPatterns) {
         if (pattern.every(v => selectedSquareIds.includes(v))) {
-          winner = player
+          winner = player;
         }
       }
     }
-
 
     return {
       moves: state.currentGameMoves,
@@ -71,23 +71,23 @@ export default class Store extends EventTarget {
       status: {
         isComplete: winner != null || state.currentGameMoves.length === 9,
         winner,
-      }
+      },
     };
   }
 
   playerMove(squareId) {
-    const stateClone = structuredClone(this.#getState())
+    const stateClone = structuredClone(this.#getState());
 
     stateClone.currentGameMoves.push({
       squareId,
       player: this.game.currentPlayer,
-    })
+    });
 
-    this.#saveState(stateClone)
+    this.#saveState(stateClone);
   }
 
   reset() {
-    const stateClone = structuredClone(this.#getState())
+    const stateClone = structuredClone(this.#getState());
 
     const { status, moves } = this.game;
 
@@ -95,19 +95,19 @@ export default class Store extends EventTarget {
       stateClone.history.currentRoundGames.push({
         moves,
         status,
-      })
+      });
     }
 
     stateClone.currentGameMoves = [];
 
-    this.#saveState(stateClone)
+    this.#saveState(stateClone);
   }
 
   newRound() {
     this.reset();
 
     const stateClone = structuredClone(this.#getState());
-    stateClone.history.allGames.push(...stateClone.history.currentRoundGames)
+    stateClone.history.allGames.push(...stateClone.history.currentRoundGames);
     stateClone.history.currentRoundGames = [];
 
     this.#saveState(stateClone);
@@ -126,15 +126,15 @@ export default class Store extends EventTarget {
 
     switch (typeof stateOrFn) {
       case 'function':
-        newState = stateOrFn(prevState)
+        newState = stateOrFn(prevState);
         break;
       case 'object':
         newState = stateOrFn;
         break;
       default:
-        throw new Error('Invalid argument passed to saveState')
+        throw new Error('Invalid argument passed to saveState');
     }
-    window.localStorage.setItem(this.storageKey, JSON.stringify(newState))
+    window.localStorage.setItem(this.storageKey, JSON.stringify(newState));
     this.dispatchEvent(new Event('statechange'));
   }
 }
--- ts/view.ts
+++ ts/view.ts
@@ -1,12 +1,6 @@
 export default class View {
-
-  $ = {
-  }
-  $$ = {
-
-  }
-
-
+  $ = {};
+  $$ = {};
 
   constructor() {
     this.$.menu = this.#qs('[data-id="menu"]');
@@ -20,8 +14,8 @@ export default class View {
     this.$.turn = this.#qs('[data-id="turn"]');
     this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
     this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
-    this.$.ties = this.#qs('[data-id="ties"]')
-    this.$.grid = this.#qs('[data-id="grid"]')
+    this.$.ties = this.#qs('[data-id="ties"]');
+    this.$.grid = this.#qs('[data-id="grid"]');
 
     this.$$.squares = this.#qsAll('[data-id="square"]');
 
@@ -32,9 +26,12 @@ export default class View {
   }
 
   render(game, stats) {
-
     const { playerWithStats, ties } = stats;
-    const { moves, currentPlayer, status: { isComplete, winner } } = game;
+    const {
+      moves,
+      currentPlayer,
+      status: { isComplete, winner },
+    } = game;
 
     this.#closeAll();
     this.#clearMoves();
@@ -44,21 +41,20 @@ export default class View {
     this.#initializeMoves(moves);
 
     if (isComplete) {
-      this.#openModal(winner ? `${winner.name} is the winner` : `It's a tie.`)
+      this.#openModal(winner ? `${winner.name} is the winner` : `It's a tie.`);
       return;
     }
     this.#setTurnIndicator(currentPlayer);
   }
 
-
-  /* 
-  *Register all the event listeners
-  */
+  /*
+   *Register all the event listeners
+   */
 
   bindGameResetEvent(handler) {
-    this.$.resetBtn.addEventListener('click', handler)
+    this.$.resetBtn.addEventListener('click', handler);
 
-    this.$.modalBtn.addEventListener('click', handler)
+    this.$.modalBtn.addEventListener('click', handler);
   }
 
   bindNewRoundEvent(handler) {
@@ -66,74 +62,65 @@ export default class View {
   }
 
   bindPlayerMoveEvent(handler) {
-    this.#delegate(this.$.ggrid, '[data-id="square"]', 'click', handler)
+    this.#delegate(this.$.ggrid, '[data-id="square"]', 'click', handler);
     // this.$$.squares.forEach(square => {
     //   square.addEventListener('click', () => handler(square))
     // });
   }
 
-  /* 
-  * DOM helper methods
-  */
+  /*
+   * DOM helper methods
+   */
 
   #updateScoreBoard(p1Wins, p2Wins, ties) {
-
     this.$.p1Wins.innerText = `${p1Wins} wins`;
     this.$.p2Wins.innerText = `${p2Wins} wins`;
     this.$.ties.innerText = `${ties} ties`;
   }
-
 
   #openModal(message) {
     this.$.modal.classList.remove('hidden');
     this.$.modalText.innerText = message;
   }
 
-
-
   #closeAll() {
     this.#closeModal();
     this.#closeMenu();
   }
 
-
   #clearMoves() {
     this.$$.squares.forEach(square => {
-      square.replaceChildren()
-    })
+      square.replaceChildren();
+    });
   }
 
   #initializeMoves(moves) {
     this.$$.squares.forEach(square => {
-      const existingMove = moves.find(move => move.squareId === +square.id)
+      const existingMove = moves.find(move => move.squareId === +square.id);
 
       if (existingMove) {
-        this.#handlePlayerMove(square, existingMove.player)
+        this.#handlePlayerMove(square, existingMove.player);
       }
-    })
+    });
   }
-
 
   #closeModal() {
-    this.$.modal.classList.add('hidden')
+    this.$.modal.classList.add('hidden');
   }
-
 
   #closeMenu() {
     this.$.menuItems.classList.add('hidden');
     this.$.menuBtn.classList.remove('border');
 
+    const icon = this.$.menuBtn.querySelector('i');
 
-    const icon = this.$.menuBtn.querySelector('i')
-
-    icon.classList.add('fa-chevron-down')
-    icon.classList.remove('fa-chevron-up')
+    icon.classList.add('fa-chevron-down');
+    icon.classList.remove('fa-chevron-up');
   }
 
   #toggleMenu() {
     this.$.menuItems.classList.toggle('hidden');
-    this.$.menuBtn.classList.toggle('border')
-
+    this.$.menuBtn.classList.toggle('border');
   }
 
   #handlePlayerMove(squareEl, player) {
@@ -142,7 +129,6 @@ export default class View {
     squareEl.replaceChildren(icon);
   }
 
-
   // player = 1 | 2
   #setTurnIndicator(player) {
     const icon = document.createElement('i');
@@ -150,35 +136,32 @@ export default class View {
 
     icon.classList.add('fa-solid', player.iconClass, player.colorClass);
 
-    label.classList.add(player.colorClass)
-    label.innerText = `${player.name} you're up!`
+    label.classList.add(player.colorClass);
+    label.innerText = `${player.name} you're up!`;
 
-    this.$.turn.replaceChildren(icon, label)
+    this.$.turn.replaceChildren(icon, label);
   }
 
   #qs(selector, parent) {
-    const el = parent
-      ? parent.querySelector(selector)
-      : document.querySelector(selector)
+    const el = parent ? parent.querySelector(selector) : document.querySelector(selector);
 
-    if (!el) throw new Error('Could not find new element')
+    if (!el) throw new Error('Could not find new element');
 
     return el;
   }
 
-
   #qsAll(selector) {
-    const elList = document.querySelectorAll(selector)
+    const elList = document.querySelectorAll(selector);
 
-    if (!elList) throw new Error('Could not find new element')
+    if (!elList) throw new Error('Could not find new element');
 
     return elList;
   }
 
   #delegate(el, selector, eventKey, handler) {
-    el.addEventListener(eventKey, (event) => {
+    el.addEventListener(eventKey, event => {
       if (event.target.matches(selector)) {
-        handler(event.target)
+        handler(event.target);
       }
     });
   }
